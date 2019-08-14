328963da2c55f6f5c19987638e52c4272c248d5c
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-08-14 18:46:08 -0400

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import PhotoCard from './components/photoCard.js'
 
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    console.log('first render')
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=XjJNBMkapfgtSHUbb2QEROa2lsViHxF8TBssdLUg')
+    .then(res => {
+      console.log(res.data);
+      setData(res.data)
+    })
+    .catch( (error) => {
+      console.log(error)
+    })
+}, [])
+  console.log('bottom')
   return (
     <div className="App">
-      <h1>Hello from App!</h1>
-      <PhotoCard title='Props title'
-                 url='The url for the site'
-                 explaination='Some Explaination of the photo'/>
+      <h1>Hello From Bobby's NASA Photo App!</h1>
+      <PhotoCard title={data.title}
+                 url={data.url}
+                 explaination={data.explaination}/>
     </div>
   );
 }
